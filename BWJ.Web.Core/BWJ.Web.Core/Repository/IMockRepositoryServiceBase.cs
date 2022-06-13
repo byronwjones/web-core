fd9d33cba822d4fd2ec53b81b8c2f05440a981e2
DESKTOP-LT9URFC\byron <byron@byronwjones.com>
2022-06-13 19:11:42 -0400

    Create service base for creating temporary storage repository

---
+++ BWJ.Web.Core/BWJ.Web.Core/Repository/IMockRepositoryServiceBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BWJ.Web.Core.Repository
+{
+    public interface IMockRepositoryServiceBase<T>
+    {
+        Task CreateRecord(T model);
+        Task DeleteRecord(Func<T, bool> predicate);
+        Task<T> GetRecord(Func<T, bool> predicate);
+        Task<IEnumerable<T>> GetRecords();
+        Task<IEnumerable<T>> GetRecords(Func<T, bool> predicate);
+        Task UpdateRecord(Func<T, bool> predicate, T model);
+    }
+}
---
+++ BWJ.Web.Core/BWJ.Web.Core/Repository/MockRepositoryServiceBase.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BWJ.Web.Core.Repository
+{
+    public abstract class MockRepositoryServiceBase<T> : IMockRepositoryServiceBase<T>
+    {
+        protected readonly object @lock = new object();
+        private List<T> Data = new List<T>();
+
+        public async Task<IEnumerable<T>> GetRecords()
+        {
+            List<T> data = null;
+            lock (@lock)
+            {
+                data = Data;
+            }
+            return await Task.FromResult(Data);
+        }
+
+        public async Task<IEnumerable<T>> GetRecords(Func<T, bool> predicate)
+        {
+            IEnumerable<T> data = null;
+            lock (@lock)
+            {
+                data = Data.Where(predicate);
+            }
+
+            return await Task.FromResult(data);
+        }
+
+        public async Task<T> GetRecord(Func<T, bool> predicate)
+        {
+            T data = default(T);
+            lock (@lock)
+            {
+                data = Data.FirstOrDefault(predicate);
+            }
+
+            return await Task.FromResult(data);
+        }
+
+        public async Task CreateRecord(T model)
+        {
+            lock (@lock)
+            {
+                Data.Add(model);
+            }
+            await Task.CompletedTask;
+        }
+
+        public async Task UpdateRecord(Func<T, bool> predicate, T model)
+        {
+            lock (@lock)
+            {
+                var old = Data.FirstOrDefault(predicate);
+                if (old != null)
+                {
+                    Data.Remove(old);
+                    Data.Add(model);
+                }
+            }
+            await Task.CompletedTask;
+        }
+
+        public async Task DeleteRecord(Func<T, bool> predicate)
+        {
+            lock (@lock)
+            {
+                var target = Data.FirstOrDefault(predicate);
+                if (target != null)
+                {
+                    Data.Remove(target);
+                }
+            }
+            await Task.CompletedTask;
+        }
+    }
+}
